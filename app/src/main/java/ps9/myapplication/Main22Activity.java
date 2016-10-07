814ef6d27d22f31f3793bb4d373e284dd38d4e0f
FrodoBagins <martinez199509@gmail.com>
2016-10-07 17:42:05 +0200

    17:41 7 paździerznika

--- app/src/main/java/ps9/myapplication/Main22Activity.java
+++ app/src/main/java/ps9/myapplication/Main22Activity.java
@@ -5,29 +5,64 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.view.Menu;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class Main22Activity extends Activity {
-    private Button btnOpenNewActivity;
-    // private Button btnOpenNextActivity
+
+    Button button1;
+    String text;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main22);
-
-        btnOpenNewActivity = (Button) findViewById(R.id.btnOpenNewActivity);
-        btnOpenNewActivity.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        addListenerOnButton();
 
 
-                onBackPressed();
-
-                //  Intent startAnotherActivity = new Intent(getApplicationContext(), MainActivity.class);
-                // startActivity(startAnotherActivity);
-            }
-        });
     }
 
+  /*  public void sendMessage(View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
+
+    }  */
+
+    public void addListenerOnButton() {
+
+        button1 = (Button) findViewById(R.id.buttoncalculate);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                EditText editText = (EditText)findViewById(R.id.editText1);
+                String text = editText.getText().toString();
+
+                Intent myIntent = new Intent(view.getContext(),Main2Activity.class);
+                myIntent.putExtra("mytext",text);
+                startActivity(myIntent);
+
+
+            }
+        });
+
+
+        }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
 
 }
--- app/src/main/java/ps9/myapplication/Main2Activity.java
+++ app/src/main/java/ps9/myapplication/Main2Activity.java
@@ -1,14 +1,18 @@
 package ps9.myapplication;
 
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Main2Activity extends Activity {
     private Button btnOpenNewActivity;
     private Button btnOpenNextActivity;
+    TextView mTextview;
 
 
     @Override
@@ -20,34 +24,35 @@ public class Main2Activity extends Activity {
         btnOpenNewActivity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-
-              //  onBackPressed();
-
                 Intent startAnotherActivity = new Intent(getApplicationContext(), Main22Activity.class);
                 startActivity(startAnotherActivity);
+
+
+
+
             }
         });
 
-       // btnOpenNextActivity = (Button) findViewById(R.id.btnOpenNextActivity);
-     //   btnOpenNextActivity.setOnClickListener(new View.OnClickListener());
+
 
 
         btnOpenNextActivity = (Button) findViewById(R.id.btnOpenNextActivity);
         btnOpenNextActivity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-
                   onBackPressed();
 
-               // Intent startAnotherActivity = new Intent(getApplicationContext(), Main22Activity.class);
-              //  startActivity(startAnotherActivity);
             }
         });
 
 
+        mTextview = (TextView)findViewById(R.id.textView1);
 
+        mTextview.setText(getIntent().getStringExtra("mytext"));
 
     }
+
+
 
 
 }
